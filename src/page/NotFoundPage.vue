dca63be204db033b537ff78ec39784be5c911376
zgccrui <zgccrui@outlook.com>
2023-05-23 08:23:30 +0800

    修改404为不自动跳转

--- src/page/NotFoundPage.vue
+++ src/page/NotFoundPage.vue
@@ -4,28 +4,13 @@ import {useGoToHome} from "@/router/goToRouter.js";
 import {ref} from "@vue/reactivity";
 import {onMounted} from "vue";
 
-onMounted(() => {
-    countDown();
-})
-
 const goToHome = useGoToHome();
-const remaining_seconds = ref(6);
-
-//倒计时
-const countDown = () => {
-    if (remaining_seconds.value > 1) {
-        remaining_seconds.value--;
-        setTimeout(countDown, 1000);
-    } else {
-        goToHome();
-    }
-}
 
 </script>
 
 <template>
     <div id="notFoundPage">
-        <h1>页面未找到，将在{{ remaining_seconds }}秒后跳转到主页面</h1>
+        <h1>404NotFound</h1>
         <el-button @click="goToHome">回到主页</el-button>
     </div>
 </template>
